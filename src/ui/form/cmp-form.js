efc86debc041720f4f77b2f816358d36f124e991
int <yuskovss@gmail.com>
2019-12-13 00:39:31 +0400

    feat: add style for card-form

--- src/ui/form/cmp-form.js
+++ src/ui/form/cmp-form.js
@@ -10,7 +10,7 @@ export default class FormCard extends React.Component {
             for="card-number" 
             className="card-item__number selected"
           >
-            ####&nbsp;&nbsp;&nbsp;&nbsp;####&nbsp;&nbsp;&nbsp;&nbsp;####&nbsp;&nbsp;&nbsp;&nbsp;####
+            #### #### #### ####
           </label>
           <label for="card-holder" className="card-item__holder-wrapper selected">
             <div className="card-item__field-caption">Card Holder</div>
@@ -23,20 +23,51 @@ export default class FormCard extends React.Component {
         </div>
 
         <div className="card-form">
-          <label>Card Number
-            <input type="text" id="card-number" className="card-form__number"></input>
+          <label className="card-form__caption">Card Number
+            <input type="text" id="card-number" className="card-form__input"></input>
           </label>
-          <label>Card Holder
-            <input type="text" id="card-holder" className="card-form__holder"></input>
+          <label className="card-form__caption">Card Holders
+            <input type="text" id="card-holder" className="card-form__input"></input>
           </label>
-          <label>Expires Date
-            <input type="text" id="card-expires-month" className="card-form__expires"></input>
-            <input type="text" id="card-expires-year" className="card-form__expires"></input>
-          </label>
-          <label>CVV
-            <input type="text" id="card-cvv" className="card-form__cvv"></input>
-          </label>
-          <input type="button" className="card-form__button" value="submit"></input>
+          <div className="card-form__end-row">
+            <label className="card-form__caption">Expires Date
+              <div className="card-form__expires-container">
+                <select id="card-expires-month" className="card-form__input card-form__expires">
+                  <option value="" disabled="disabled" selected="selected">Month</option>
+                  <option value="1">01</option>
+                  <option value="2">02</option>
+                  <option value="3">03</option>
+                  <option value="4">04</option>
+                  <option value="5">05</option>
+                  <option value="6">06</option>
+                  <option value="7">07</option>
+                  <option value="8">08</option>
+                  <option value="9">09</option>
+                  <option value="10">10</option>
+                  <option value="11">11</option>
+                  <option value="12">12</option>
+                </select>
+                <select id="card-expires-year" className="card-form__input card-form__expires">
+                  <option value="" disabled="disabled" selected="selected">Year</option>
+                  <option value="2020">2020</option>
+                  <option value="2021">2021</option>
+                  <option value="2022">2022</option>
+                  <option value="2023">2023</option>
+                  <option value="2024">2024</option>
+                  <option value="2025">2025</option>
+                  <option value="2026">2026</option>
+                  <option value="2027">2027</option>
+                  <option value="2028">2028</option>
+                  <option value="2029">2029</option>
+                  <option value="2030">2030</option>
+                </select>
+              </div>
+            </label>
+            <label className="card-form__caption">CVV
+              <input type="text" id="card-cvv" className="card-form__input card-form__cvv"></input>
+            </label>
+          </div>
+          <input type="button" className="card-form__button" value="Submit"></input>
         </div>
       </div>
     );
