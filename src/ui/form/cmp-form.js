94af4e6aa87c5d9c1aa682c5b565d96f910f8a5c
int <yuskovss@gmail.com>
2019-12-16 21:23:53 +0400

    refactor: change css and html for card-form

--- src/ui/form/cmp-form.js
+++ src/ui/form/cmp-form.js
@@ -23,16 +23,19 @@ export default class FormCard extends React.Component {
         </div>
 
         <div className="card-form">
-          <label className="card-form__caption">Card Number
+          <div className="card-form__number card-form__row">
+            <label className="card-form__caption">Card Number</label>
             <input type="text" id="card-number" className="card-form__input"></input>
-          </label>
-          <label className="card-form__caption">Card Holders
+          </div>
+          <div className="card-form__holder card-form__row">
+            <label className="card-form__caption">Card Holders</label>
             <input type="text" id="card-holder" className="card-form__input"></input>
-          </label>
-          <div className="card-form__end-row">
-            <label className="card-form__caption">Expires Date
+          </div>
+          <div className="card-form__end-row card-form__row">
+            <div className="card-form__expires">
+              <label className="card-form__caption">Expires Date</label>
               <div className="card-form__expires-container">
-                <select id="card-expires-month" className="card-form__input card-form__expires">
+                <select id="card-expires-month" className="card-form__input card-form__select">
                   <option value="" disabled="disabled" selected="selected">Month</option>
                   <option value="1">01</option>
                   <option value="2">02</option>
@@ -47,25 +50,26 @@ export default class FormCard extends React.Component {
                   <option value="11">11</option>
                   <option value="12">12</option>
                 </select>
-                <select id="card-expires-year" className="card-form__input card-form__expires">
-                  <option value="" disabled="disabled" selected="selected">Year</option>
-                  <option value="2020">2020</option>
-                  <option value="2021">2021</option>
-                  <option value="2022">2022</option>
-                  <option value="2023">2023</option>
-                  <option value="2024">2024</option>
-                  <option value="2025">2025</option>
-                  <option value="2026">2026</option>
-                  <option value="2027">2027</option>
-                  <option value="2028">2028</option>
-                  <option value="2029">2029</option>
-                  <option value="2030">2030</option>
-                </select>
+                <select id="card-expires-year" className="card-form__input card-form__select">
+                    <option value="" disabled="disabled" selected="selected">Year</option>
+                    <option value="2020">2020</option>
+                    <option value="2021">2021</option>
+                    <option value="2022">2022</option>
+                    <option value="2023">2023</option>
+                    <option value="2024">2024</option>
+                    <option value="2025">2025</option>
+                    <option value="2026">2026</option>
+                    <option value="2027">2027</option>
+                    <option value="2028">2028</option>
+                    <option value="2029">2029</option>
+                    <option value="2030">2030</option>
+                  </select>
               </div>
-            </label>
-            <label className="card-form__caption">CVV
-              <input type="text" id="card-cvv" className="card-form__input card-form__cvv"></input>
-            </label>
+            </div>
+            <div className="card-form__cvv">
+              <label className="card-form__caption">CVV</label>
+              <input type="text" id="card-cvv" className="card-form__input"></input>
+            </div>
           </div>
           <input type="button" className="card-form__button" value="Submit"></input>
         </div>
